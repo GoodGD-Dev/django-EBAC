58648ff5a627586646566c0a388f80e9b9d07d7f
GoodGD <dionatha.work@gmail.com>
2025-01-23 18:17:01 -0300

    templates

--- blog/factories.py
+++ blog/factories.py
@@ -6,31 +6,33 @@ from django.utils.timezone import now
 
 from blog.models import Post
 
-faker = FakerFactory.create()
+faker = FakerFactory.create() # Create a faker instance
 
-class UserFactory(factory.django.DjangoModelFactory):
+
+class UserFactory(factory.django.DjangoModelFactory): # Create a UserFactory class
     class Meta:
         model = User
 
-    email = factory.Faker("safe_email")
-    username = factory.LazyAttribute(lambda x: faker.name())
+    email = factory.Faker("safe_email") # Use faker to generate an email
+    username = factory.LazyAttribute(lambda x: faker.name()) # Use faker to generate a name
+
 
     @classmethod
-    def _prepare(cls, create, **kwargs):
-        password = kwargs.pop("password", None)
-        user = super(UserFactory, cls)._prepare(create, **kwargs)
+    def _prepare(cls, create, **kwargs): # Prepare the user
+        password = kwargs.pop("password", None) # Pop the password from the kwargs
+        user = super(UserFactory, cls)._prepare(create, **kwargs) #prepare the user
         if password:
             user.set_password(password)
             if create:
                 user.save()
-        return user
-
-class PostFactory(factory.django.DjangoModelFactory):
-    title = factory.LazyAttribute(lambda x: faker.sentence())
-    created_on = factory.LazyAttribute(lambda x: now())
-    author = factory.SubFactory(UserFactory)
+            return user
+        
+class PostFactory(factory.django.DjangoModelFactory): # Create a PostFactory class
+    title = factory.LazyAttribute(lambda x: faker.sentence()) # Use faker to generate a sentence
+    created_on = factory.LazyAttribute(lambda x: now()) # Use the current time
+    author = factory.SubFactory(UserFactory) # Use the UserFactory to create an author
     status = 0
 
-    class Meta:
-        model = Post
 
+    class Meta:
+        model = Post # The model to Post
